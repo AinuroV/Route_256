47cc243bd4f47a8ff7e1f0f75762fe34ea411c2e
Airat Nurov <zinnurov.airat@inbox.ru>
2024-02-09 15:53:13 +0300

    Решение 1 задачи

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = int.Parse(Console.ReadLine());
+            
+            string ship = "4 3 3 2 2 2 1 1 1 1";
+            var response = "";
+
+            for (var i = 0; i < count; i++)
+            {
+                var ships = Console.ReadLine().Split().ToList();
+
+                ships.Sort((a, b) => a.CompareTo(b) * -1);
+
+                response += "\n" + (String.Join(" ", ships.ToArray()) == ship ? "YES" : "NO");
+                
+            }
+            Console.WriteLine(response.Trim());
+        }
+    }
+
+}
